419c674b79a49b1afe3169aed02aed3c81565f56
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:04:43 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "sentinel_data_connector_aws_s3s_id" {
+  description = "Map of id values across all sentinel_data_connector_aws_s3s, keyed the same as var.sentinel_data_connector_aws_s3s"
+  value       = { for k, v in azurerm_sentinel_data_connector_aws_s3.sentinel_data_connector_aws_s3s : k => v.id }
+}
 output "sentinel_data_connector_aws_s3s_aws_role_arn" {
   description = "Map of aws_role_arn values across all sentinel_data_connector_aws_s3s, keyed the same as var.sentinel_data_connector_aws_s3s"
   value       = { for k, v in azurerm_sentinel_data_connector_aws_s3.sentinel_data_connector_aws_s3s : k => v.aws_role_arn }
